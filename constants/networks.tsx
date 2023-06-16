f16045d63fd98530bfc66543eade23be39d5efa9
stake-machine <80471159+stake-machine@users.noreply.github.com>
2023-06-16 15:48:02 +0300

    chore: remove kusama from networks

--- constants/networks.tsx
+++ constants/networks.tsx
@@ -1,24 +1,5 @@
 export var NetworksJson = [
   {
-    name: "Kusama",
-    desc: "Kusama is a scalable network of specialized blockchains built using Substrate and nearly the same codebase as Polkadot.",
-    slug: "kusama",
-    fee: 0.05,
-    apy: null,
-    coingecko_id: "kusama",
-    symbol: "KSM",
-    logo: "https://cdn.jsdelivr.net/gh/atomiclabs/cryptocurrency-icons@bea1a9722a8c63169dcc06e86182bf2c55a76bbc/svg/icon/ksm.svg",
-    tokens: 5000,
-    usd_rate: 40,
-    staking_url: "https://polkadot.js.org/apps/#/staking",
-    validators: [
-      {
-        address: "EchB8XdgqCWS6dbBzhrgtPtrADfyQV9qRAAC9Q7dWbpYZVS",
-        name: "Kusama Validator1",
-      },
-    ],
-  },
-  {
     name: "The Graph",
     desc: "The Graph is an indexing protocol for organizing blockchain data and making it easily accessible with GraphQL.",
     slug: "thegraph",
