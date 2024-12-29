df8a06051699a7955b31e96abccfaab267cdc6f6
stake-machine <80471159+stake-machine@users.noreply.github.com>
2024-12-29 16:47:26 +0300

    fix: remove projects from sitemap

---
+++ app/sitemap.tsx
@@ -0,0 +1,21 @@
+import { MetadataRoute } from "next";
+import { NetworksJson } from "../constants/networks";
+export const dynamic = "force-static";
+export default function sitemap(): MetadataRoute.Sitemap {
+  const baseUrl = "https://stake-machine.com"; // Replace with your actual domain
+
+  // Define your static routes
+  const staticRoutes = [""].map((route) => ({
+    url: `${baseUrl}${route}`,
+    lastModified: new Date().toISOString(),
+  }));
+
+  // Define your dynamic network routes
+  const networks = NetworksJson.map((network) => network.slug);
+  const networkRoutes = networks.map((network) => ({
+    url: `${baseUrl}/networks/${network}`,
+    lastModified: new Date().toISOString(),
+  }));
+
+  return [...staticRoutes, ...networkRoutes];
+}
